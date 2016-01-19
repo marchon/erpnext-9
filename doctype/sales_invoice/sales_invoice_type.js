cf29e45d260a7468a1d07b285f690e2cda072d21
seethersan <carlos_jcez@hotmail.com>
2016-01-18 20:45:07 -0500

    modificando archivo sales_invoice_type.js

--- doctype/sales_invoice/sales_invoice_type.js
+++ doctype/sales_invoice/sales_invoice_type.js
@@ -3,17 +3,17 @@ frappe.ui.form.on("Sales Invoice", "customer",
         frappe.call({
             "method": "frappe.client.get",
             args: {
-                doctype: "Customer",
+                doctype: "Sales Invoice",
                 name: frm.doc.tax_id
             },
             callback: function (data) {
                 if (data.message.tax_id != "") {
-                    frappe.model.set_value(frm.doctype, frm.docname, "sales_type", "Factura de Venta")
-                    frappe.model.set_value(frm.doctype, frm.docname, "naming_series", "FV-004-")
+                    frappe.model.set_value(frm.doctype, frm.docname, "sales_type", "Factura de Venta");
+                    frappe.model.set_value(frm.doctype, frm.docname, "naming_series", "FV-004-");
                 }
                 else {
-                    frappe.model.set_value(frm.doctype, frm.docname, "sales_type", "Boletaa de Venta")
-                    frappe.model.set_value(frm.doctype, frm.docname, "naming_series", "BV-004-")
+                    frappe.model.set_value(frm.doctype, frm.docname, "sales_type", "Boletaa de Venta");
+                    frappe.model.set_value(frm.doctype, frm.docname, "naming_series", "BV-004-");
                 }
             }
         })
